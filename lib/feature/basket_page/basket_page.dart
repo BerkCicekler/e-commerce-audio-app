d5e47908ab877bed723e9900a5c409e3fae8b728
Berk Çiçekler <berkcicekler7@gmail.com>
2024-12-11 21:53:13 +0300

    action dialog feature added

--- lib/feature/basket_page/basket_page.dart
+++ lib/feature/basket_page/basket_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ecommerceaudio/feature/widgets/app_action_dialog.dart';
 import 'package:ecommerceaudio/feature/widgets/app_image_network.dart';
 import 'package:ecommerceaudio/product/bloc/basket_bloc/basket_bloc.dart';
 import 'package:ecommerceaudio/product/constants/app_color_constants.dart';
